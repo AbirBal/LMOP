8621b20a3e9ed86273b51a7ebfe5539ac5914b9a
AbirBal <baltiabir88@gmail.com>
2025-07-24 13:17:16 +0200

    update navbar la page active

--- navbar.js
+++ navbar.js
@@ -5,3 +5,9 @@
     navbarLinks.classList.toggle('show');
     document.body.classList.toggle('menu-open');
   });
+ const toggleBtn = document.querySelector('.menu-toggle-sub');
+  const navbarSub = document.querySelector('.navbar-sub');
+
+  toggleBtn.addEventListener('click', () => {
+    navbarSub.classList.toggle('active');
+  });
